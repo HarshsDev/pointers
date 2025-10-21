3319321ffbee6e903ef78f86e26aff991087bd66
Harsh Sawarkar <harshsawarkar111@gmail.com>
2025-10-21 21:36:08 +0530

    Initialize project structure with Cargo configuration and implement RefCell and Cell types

---
+++ src/RefCell.rs
@@ -0,0 +1,119 @@
+use std::{cell::UnsafeCell, ops::{Deref, DerefMut}};
+use crate::Cell;
+
+#[derive(Copy, Clone)]
+enum RefType {
+    Unshared,
+    Shared(usize),
+    Exclusive,
+}
+
+pub struct RefCell<T>{
+value: UnsafeCell<T>,
+reference: Cell<RefType>
+}
+
+impl<T> RefCell<T> {
+    pub fn new(value: T) -> Self {
+        RefCell {
+            value: UnsafeCell::new(value),
+            reference: Cell::new(RefType::Unshared)
+        }
+    }
+
+     pub fn borrow(&self) -> Option<&T> {
+       match self.reference.get() {
+         RefType::Unshared => {
+            self.reference.set(RefType::Shared(1));
+            Some(unsafe {
+                &*self.value.get()
+            })
+         }, 
+         RefType::Shared(n) => {
+            self.reference.set(RefType::Shared(n+1));
+            Some(unsafe {
+                &*self.value.get()
+            })
+         },
+         RefType::Exclusive => {
+            None
+         }
+       }
+     }
+
+     pub fn borrow_mut(&self) -> Option<&mut T> {
+        if let RefType::Unshared = self.reference.get() {
+            self.reference.set(RefType::Exclusive);
+            unsafe {
+                Some(&mut *self.value.get())
+            }
+        } else {
+            None
+        }
+     }
+     
+}
+
+pub struct Ref<'refCell,T> {
+    refCell: &'refCell RefCell<T>,
+}
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        match self.refCell.reference.get() {
+            RefType::Unshared | RefType::Exclusive => unreachable!(),
+            RefType::Shared(1) => {
+                self.refCell.reference.set(RefType::Unshared);
+            },
+            RefType::Shared(n) => {
+                self.refCell.reference.set(RefType::Shared(n-1));
+            }
+        }
+    }
+}
+
+impl<T> std::ops::Deref for Ref<'_,T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &*self.refCell.value.get()
+        }
+    }
+}
+
+pub struct RefMut<'refCell, T> {
+    refCell: &'refCell RefCell<T>,
+}
+
+impl<T> Drop for RefMut<'_,T> {
+    fn drop(&mut self) {
+        match self.refCell.reference.get() {
+            RefType::Unshared | RefType::Shared(_) => unreachable!(),
+            RefType::Exclusive => {
+                self.refCell.reference.set(RefType::Unshared);
+            }
+        }
+    }
+}
+
+impl <T> Deref for RefMut<'_,T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &*self.refCell.value.get()
+        }
+    }
+}
+
+impl <T> DerefMut for RefMut<'_, T> {
+// type Target = T;
+fn deref_mut(&mut self) -> &mut Self::Target {
+    unsafe {
+        &mut *self.refCell.value.get()
+}
+}
+}
+
+// impl<'RefCell> 
+    
+// }
---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+mod RefCell;
+use std::cell::UnsafeCell;
+
+pub struct Cell<T> {
+    value: UnsafeCell<T>
+}
+
+impl<T> Cell<T> {
+ pub fn new(value: T) -> Self {
+    Cell{
+        value: UnsafeCell::new(value)
+    }
+ } 
+
+ pub fn set(&self, value: T)  {
+    // SAFETY: we know no one else is modifying the cells value concurrently
+    // SAFETY: cause koi bhi refernce ko invalide nhi kr rahe cause hum kisko woh grant  nhi kar rahe using only copy
+    unsafe {
+        *self.value.get() = value;
+    }
+ }
+
+ pub fn get(&self) -> T 
+ where T:Copy {
+    unsafe {
+        *self.value.get()
+    }
+ }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
+}
